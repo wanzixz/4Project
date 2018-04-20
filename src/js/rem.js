0b2aba16abc5d30aa09abb17e7d2f0f37861f083
wanzixz <1471299010@qq.com>
2018-04-20 19:48:13 +0800

    d

--- src/js/rem.js
+++ src/js/rem.js
@@ -5,10 +5,10 @@
     recalc = function () {
       var clientWidth = docEl.clientWidth;
       if (!clientWidth) return;
-      if (clientWidth >= 750) {
+      if (clientWidth >= 640) {
         docEl.style.fontSize = '100px';
       } else {
-        docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+        docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
       }
     };
   if (!doc.addEventListener) return;
